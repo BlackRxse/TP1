b3c148e00c9e2bacf66419639118869078ae89cf
Noelia Mustaff <90294586+BlackRxse@users.noreply.github.com>
2021-11-26 18:13:15 -0300

    Add files via upload

---
+++ funciones.h
@@ -0,0 +1,15 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+float sumar(float a, float b);
+
+float restar(float a, float b);
+
+float multiplicar(float a, float b);
+
+float dividir(float a, float b);
+
+int calcularFactorial(int a);
+
+
+#endif // FUNCIONES_H_INCLUDED
