9d24f5581e237c47a94d263ffd2979a879415291
Noelia Mustaff <90294586+BlackRxse@users.noreply.github.com>
2021-11-28 17:37:38 -0300

    Add files via upload

---
+++ funciones.h
@@ -0,0 +1,66 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+#include <stdio.h>
+#include <stdlib.h>
+
+int sumar(int a, int b);
+
+int restar(int a, int b);
+
+int multiplicar(int a, int b);
+
+int dividir(int a, int b);
+
+int calcularFactorial(int a);
+
+int sumar(int a, int b)
+{
+    return a + b;
+}
+
+int restar( int a, int b)
+{
+    return a - b;
+}
+
+int multiplicar(int a, int b)
+{
+    return a * b;
+}
+
+int dividir(int a, int b)
+{ 
+    if (a > 0 && b == 0)
+    {
+        return -1;
+    }
+    return a / b;
+}
+
+int calcularFactorial(int a)
+{
+    int factorial;
+    if (a == 1 || a == 0)
+    {
+        return 1;
+    }
+    factorial = a * calcularFactorial(a - 1);
+    return factorial;
+}
+
+int validarNumero(char string[])
+{
+    for (int i = 0; i < strlen(string); i++)
+    {
+        if (string[i] != '\0')
+        {
+            if (string[i] < '0' || string[i] > '9')
+            {
+                return 0;
+            }
+            return 1;
+        }
+    }
+}
+
+#endif // FUNCIONES_H_INCLUDED
